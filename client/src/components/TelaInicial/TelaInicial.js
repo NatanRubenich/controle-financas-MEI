7c42d891bf95e1c351368c3872463388a7b71d3b
Antonio José <antoniojosegteixeira@gmail.com>
2021-07-13 15:36:30 -0400

    Hero remodelado

--- client/src/components/TelaInicial/TelaInicial.js
+++ client/src/components/TelaInicial/TelaInicial.js
@@ -5,59 +5,16 @@ import SideNav from '../SideNav/SideNav';
 const TelaInicial = () => {
 
   return (
-    <div className="row m-0 min-vh-100 hero-image align-content-center justify-content-end text-white">
-      <div className="col-6 me-5">
-        <h1 className="my-3">CONTROLE MEI</h1>
-        <h5 className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h5>
-        
-        <div className="row align-items-center justify-content-center">
-          <div className="col-0">
-            <div className="row my-2r">
-              <div className="col-0 col-md-6 p-1">
-                <div className="d-inline-flex">
-                  <div className="icone text-dark m-1">
-                    <i className="fas fa-clock"></i>
-                  </div>
-                  <h6 className="align-self-center m-1 ">Lorem ipsum dolor sit amet</h6>
-                </div>
-              </div>
-
-              <div className="col-0 col-md-6 p-1">
-                <div className="d-inline-flex">
-                  <div className="icone text-dark m-1">
-                    <i className="fas fa-clock"></i>
-                  </div>
-                  <h6 className="align-self-center m-1 ">Lorem ipsum dolor sit amet</h6>
-                </div>
-              </div>
-            </div>
-
-            <div className="row my-2">
-              <div className="col-0 col-md-6 p-1">
-                <div className="d-inline-flex">
-                  <div className="icone text-dark m-1">
-                    <i className="fas fa-clock"></i>
-                  </div>
-                  <h6 className="align-self-center m-1 ">Lorem ipsum dolor sit amet</h6>
-                </div>
-              </div>
-
-              <div className="col-0 col-md-6 p-1">
-                <div className="d-inline-flex">
-                  <div className="icone text-dark m-1">
-                    <i className="fas fa-clock"></i>
-                  </div>
-                  <h6 className="align-self-center m-1 ">Lorem ipsum dolor sit amet</h6>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="row m-0 min-vh-100 hero-image align-items-center">
+      <div className="blind"></div>
+      <div className="col-1 d-none d-md-flex"></div>
+      <div className="col-9 col-md-5 mx-auto mx-md-1 text-center text-md-start z-index2">
+        <h1 className="my-4 display-3">Controle MEI</h1>
+        <p className="my-4 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <div className="d-flex flex-column flex-md-row gap-2 justify-content-start mb-4 ">
+          <button class="btn btn-success py-2 me-md-2" type="button">Fazer Login</button>
+          <button class="btn btn-dark py-2" type="button">Inscreva-se</button>
         </div>
-
-
-
-
-
       </div>
     </div>
   
