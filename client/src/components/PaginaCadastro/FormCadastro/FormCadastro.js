d5732b839bf9a05ce093d2cce912d34f7dda977b
Antonio José <antoniojosegteixeira@gmail.com>
2021-07-17 12:01:21 -0400

    Lógica de formulário implementada

--- client/src/components/PaginaCadastro/FormCadastro/FormCadastro.js
+++ client/src/components/PaginaCadastro/FormCadastro/FormCadastro.js
@@ -14,18 +14,17 @@ const FormCadastro = () => {
     senhaConf: ""
   });
 
-
   return (            
     <form class="row">
 
-      <FormInput nome="nome" titulo="Nome" tipo="text" classe="col-md-6"/>
-      <FormInput nome="sobrenome" titulo="Sobrenome" tipo="text" classe="col-md-6"/>
-      <FormInput nome="nomeEmpresa" titulo="Nome da Empresa" tipo="text" classe="col-md-7"/>
-      <FormInput nome="cnpj" titulo="CNPJ" tipo="text" classe="col-md-5"/>
-      <FormInput nome="email" titulo="Email" tipo="email" classe="col-md-7"/>
-      <FormInput nome="telefone" titulo="Telefone" tipo="text" classe="col-md-5"/>
-      <FormInput nome="senha" titulo="Senha" tipo="password" classe="col-md-6"/>
-      <FormInput nome="senhaConf" titulo="Confirme a Senha" tipo="password" classe="col-md-6"/>
+      <FormInput nome="nome" titulo="Nome" tipo="text" classe="col-md-6" form={form} setForm={setForm}/>
+      <FormInput nome="sobrenome" titulo="Sobrenome" tipo="text" classe="col-md-6" form={form} setForm={setForm}/>
+      <FormInput nome="nomeEmpresa" titulo="Nome da Empresa" tipo="text" classe="col-md-7"  form={form} setForm={setForm}/>
+      <FormInput nome="cnpj" titulo="CNPJ" tipo="text" classe="col-md-5"  form={form} setForm={setForm}/>
+      <FormInput nome="email" titulo="Email" tipo="email" classe="col-md-7"  form={form} setForm={setForm}/>
+      <FormInput nome="telefone" titulo="Telefone" tipo="text" classe="col-md-5"  form={form} setForm={setForm}/>
+      <FormInput nome="senha" titulo="Senha" tipo="password" classe="col-md-6"  form={form} setForm={setForm}/>
+      <FormInput nome="senhaConf" titulo="Confirme a Senha" tipo="password" classe="col-md-6"  form={form} setForm={setForm}/>
       
       <div class="col-0 p-4">
         <div className="row">
