b62bf95daee36040f191d35832b625cfb88aa859
Antonio José <antoniojosegteixeira@gmail.com>
2021-07-19 11:57:21 -0400

    ENV e dotenv adicionado

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
 import Home from './routes/crud.js';
 
+// ENV
+const dotenvResultado = dotenv.config();
+if (dotenvResultado.error) {
+  throw dotenvResultado.error;
+}
+
 // Express
 const app = express();
-const router = express.Router();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cors());
 
 
-
 app.use('/', Home); 
 
-app.listen(5000, () => console.log('Server rodando na porta', 5000))
+app.listen(process.env.ROTA, () => console.log('Server rodando na porta', process.env.ROTA))
