bed5a972a8734f21e57d30d23a292ec679f016f7
Antonio José <antoniojosegteixeira@gmail.com>
2021-07-23 14:56:53 -0400

    Moeda formatada

--- client/src/components/PaginaTabela/TabelaCrud/ItemCrud/ItemCrud.js
+++ client/src/components/PaginaTabela/TabelaCrud/ItemCrud/ItemCrud.js
@@ -4,19 +4,31 @@ const ItemCrud = ({ dados }) => {
 
   const data = new Date(dados.dataVenda).toLocaleString('pt-BR').split(' ');
   
+  const formatarTipoVenda = () => {
+    let resposta = dados.tipoVenda;
+    if(resposta === 'servico') {
+      return resposta = 'Serviço';
+    } else if (resposta === 'produto') {
+      return resposta = 'Produto';
+    }
+    return resposta;
+  }
 
+  const formatarMoeda = (valor) => {
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  }
 
   return (
     <tr className="td-registros">
       <td>{dados.nomeCliente}</td>
-      <td>{dados.tipoVenda}</td>
+      <td>{formatarTipoVenda()}</td>
       <td>{data[0]}</td>
       <td>{dados.quantidade}</td>
-      <td>{dados.valorUnitario}</td>
+      <td>{formatarMoeda(dados.valorUnitario)}</td>
       <td>{dados.descricao}</td>
       <td>{dados.desconto} %</td>
       <td>{dados.notaFiscal ? "Sim" : "Não"}</td>
-      <td>{dados.valorFinal}</td>
+      <td>{formatarMoeda(dados.valorFinal)}</td>
 
       <td className="acoes">
         <div className="d-flex"> 
